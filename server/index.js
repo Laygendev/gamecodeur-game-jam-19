ce5d4361820858c5573c614d3072e652332ff74b
Laygendev <37774381+Laygendev@users.noreply.github.com>
2019-03-13 22:13:01 +0100

    Update

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var TankServer = require('./server.js');
 var tankServer = new TankServer(io);
+var path = require('path');
 
-// app.use(express.static('../asset'));
-// app.use(express.static('../src'));
+app.use(express.static('../asset'));
+app.use(express.static('../src'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/../index.html');
+  res.sendFile(path.resolve('index.html'));
 });
 
 io.on('connection', function(socket){
