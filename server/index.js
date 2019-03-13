31e83d6c1d1b095e14d8f7ca3e9b1ef892e35366
Laygendev <37774381+Laygendev@users.noreply.github.com>
2019-03-13 22:34:49 +0100

    test

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, {
-  pingInterval: 1000,
-  upgradeTimeout: 5000
+  pingInterval: 2000
 });
 var TankServer = require('./server.js');
 var tankServer = new TankServer(io);
