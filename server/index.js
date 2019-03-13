33a44a6140744d9cec753ab6267d050983d60209
Laygendev <37774381+Laygendev@users.noreply.github.com>
2019-03-13 22:08:01 +0100

    Update

--- server/index.js
+++ server/index.js
@@ -1,72 +1,20 @@
-var version = "0.1";
-var port    = 8080;
-var server  = require("http");
-var fs      = require("fs");
-const url = require('url');
-const path = require('path');
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var TankServer = require('./server.js');
-
-// maps file extention to MIME types
-const mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.wav': 'audio/wav',
-  '.mp3': 'audio/mpeg',
-  '.svg': 'image/svg+xml',
-  '.pdf': 'application/pdf',
-  '.doc': 'application/msword',
-  '.eot': 'appliaction/vnd.ms-fontobject',
-  '.ttf': 'aplication/font-sfnt'
-};
-
-server = server.createServer(function(req, res) {
-  const parsedUrl = url.parse(req.url);
-
-  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
-  let pathname = path.join(__dirname, sanitizePath);
-
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
-    }
-
-    // if is a directory, then look for index.html
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '../index.html';
-    }
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-        const ext = path.parse(pathname).ext;
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-});
-
-var io = require("socket.io")(server, {
-  'pingInterval': 2000,
-});
 var tankServer = new TankServer(io);
 
-io = io.sockets.on("connection", (socket) => { tankServer.handleSocket(socket); });
+app.use(express.static('asset'));
+app.use(express.static('src'));
 
-server.listen(port);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-console.log("Server started");
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
