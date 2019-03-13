0b831dfad75a4380fa5ab7b9fb813b92bf394ad9
Laygendev <37774381+Laygendev@users.noreply.github.com>
2019-03-13 21:57:08 +0100

    Update handle page

--- server/index.js
+++ server/index.js
@@ -1,16 +1,44 @@
 var version = "0.1";
 var port    = 8080;
-var server  = require("https");
+var server  = require("http");
 var fs      = require("fs");
 
 var TankServer = require('./server.js');
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/trackball-game.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/trackball-game.com/cert.pem')
-};
+server = server.createServer(function(req, res) {
+  const parsedUrl = url.parse(req.url);
 
-server = server.createServer(options);
+  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+  let pathname = path.join(__dirname, sanitizePath);
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory, then look for index.html
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/index.html';
+    }
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+        const ext = path.parse(pathname).ext;
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+});
 
 var io = require("socket.io")(server, {
   'pingInterval': 2000,
@@ -22,17 +50,3 @@ io = io.sockets.on("connection", (socket) => { tankServer.handleSocket(socket); 
 server.listen(port);
 
 console.log("Server started");
-
-var width = 3000;
-var height = 3000;
-
-
-var dt;
-var lastframetime;
-var updateid;
-var startTime;
-var last_processed_input = [];
-var messages = [];
-var t = 0;
-var pt = 0;
-var last_ts;
