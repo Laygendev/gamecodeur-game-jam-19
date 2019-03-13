f760e438723e2f4ae209b29aa663da4c8804697e
Laygendev <37774381+Laygendev@users.noreply.github.com>
2019-03-13 22:16:41 +0100

    Update static

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ var TankServer = require('./server.js');
 var tankServer = new TankServer(io);
 var path = require('path');
 
+app.use('/lib', express.static('lib'));
+app.use('/shared', express.static('shared'));
 app.use('/asset', express.static('asset'));
 app.use('/src', express.static('src'));
 
